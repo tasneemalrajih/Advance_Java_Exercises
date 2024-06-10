9ef45ab1f83696dd903181073417978e965a1020
Tasneem Alrajih <tasneemalrajih.gmail.com>
2024-06-10 10:58:14 +0300

    ExDay10

---
+++ ExDay10/src/main/java/org/example/services/ApplicationService.java
@@ -0,0 +1,15 @@
+package org.example.services;
+
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.RequestScoped;
+
+@ApplicationScoped
+public class ApplicationService {
+
+    private int count;
+
+    public int getCount() {
+        return ++count;
+    }
+}
---
+++ ExDay10/src/main/java/org/example/services/SessionService.java
@@ -0,0 +1,33 @@
+package org.example.services;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Inject;
+import org.example.dao.DepartmentDAO;
+import org.example.models.Department;
+
+import java.io.Serializable;
+
+@SessionScoped
+public class SessionService implements Serializable {
+
+    private int count;
+
+//    @Inject
+//    DepartmentDAO dao;
+
+    public int getCount() {
+        return ++count;
+    }
+    @PostConstruct
+    public void init(){
+        System.out.println("Session object created");
+    }
+
+    @PreDestroy
+    public void Kill(){
+        System.out.println("Session object Will be Killed");
+    }
+}
